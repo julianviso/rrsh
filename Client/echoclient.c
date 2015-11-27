fd5dd8cf1bcfd02daf09de7af5d06e63741d9386
ashiramin <ashir.amin@uky.edu>
2015-11-27 14:56:31 -0500

    Auth works

--- Client/echoclient.c
+++ Client/echoclient.c
@@ -9,7 +9,7 @@ int main(int argc, char **argv)
     int clientfd, port;
     char *host, buf[MAXLINE];
     rio_t rio;
-
+    char buf2[MAXLINE];
     if (argc != 3) {
 	fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
 	exit(0);
@@ -19,7 +19,23 @@ int main(int argc, char **argv)
 
     clientfd = Open_clientfd(host, port);
     Rio_readinitb(&rio, clientfd);
+    int auth = 0;
+	printf("UserName: ");
+	Fgets(buf, MAXLINE, stdin);
+	Rio_writen(clientfd, buf, strlen(buf));
+	Rio_readlineb(&rio, buf, MAXLINE);
+//	Fputs(buf, stdout);
 
+	printf("Password: ");
+        Fgets(buf2, MAXLINE, stdin);
+	Rio_writen(clientfd, buf2, strlen(buf2));
+//	Fputs(buf, stdout);
+	Rio_readlineb(&rio, buf2, MAXLINE);
+    	Fputs(buf2, stdout);
+/*	if (strcmp("Yes\n",buf2)==0)
+	{
+	printf("YESSS\n");
+	}*/
     while (Fgets(buf, MAXLINE, stdin) != NULL) {
 	Rio_writen(clientfd, buf, strlen(buf));
 	Rio_readlineb(&rio, buf, MAXLINE);
--- Server/echo.c
+++ Server/echo.c
@@ -3,17 +3,53 @@
  */
 /* $begin echo */
 #include "csapp.h"
-
 void echo(int connfd) 
 {
-    size_t n; 
-    char buf[MAXLINE]; 
+    size_t n;
+    size_t a;
+    char buf[MAXLINE];
+    char buf2[MAXLINE];
+    char ab[4] = "YOLO"; 
     rio_t rio;
-
+    rio_t rio2;
+    pid_t pid;
     Rio_readinitb(&rio, connfd);
+
+    FILE *fp1;
+    char oneword[100];
+    char c;
+	Rio_readlineb(&rio, buf, MAXLINE);
+	Rio_writen(connfd,"Yes\n",4);
+	Rio_readlineb(&rio, buf2, MAXLINE);
+      //  Rio_writen(connfd,"Yes\n",4);
+        fp1 = fopen("rrshusers.txt","r");
+
+   do {
+      c = fscanf(fp1,"%s",oneword); /* got one word from the file */
+      printf("%s\n",oneword);       /* display it on the monitor  */
+	if (strcmp(oneword,strtok(buf,"\n"))==0)
+	{	
+		c = fscanf(fp1,"%s",oneword);
+		if (strcmp(oneword,strtok(buf2,"\n"))==0)
+		{
+		   Rio_writen(connfd,"Yessss\n",7);
+		}
+	}
+   } while (c != EOF);              /* repeat until EOF           */
+
+    fclose(fp1);
+
+
     while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) { //line:netp:echo:eof
 	printf("server received %d bytes\n", (int)n);
-	Rio_writen(connfd, buf, n);
-    }
+	 if (strcmp("abc\n",buf) ==0 )
+	{
+	  Rio_writen(connfd,"Yes\n",4);
+	}
+	else
+	{
+		Rio_writen(connfd,"No\n",3);	
+	}
+	}
 }
 /* $end echo */
