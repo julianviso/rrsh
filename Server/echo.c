b634c4b7ac144851a434c9335ac86ec479cebe74
ashiramin <ashir.amin@uky.edu>
2015-11-27 15:17:29 -0500

    fixed login bugs

--- Server/echo.c
+++ Server/echo.c
@@ -9,7 +9,6 @@ void echo(int connfd)
     size_t a;
     char buf[MAXLINE];
     char buf2[MAXLINE];
-    char ab[4] = "YOLO"; 
     rio_t rio;
     rio_t rio2;
     pid_t pid;
@@ -23,20 +22,30 @@ void echo(int connfd)
 	Rio_readlineb(&rio, buf2, MAXLINE);
       //  Rio_writen(connfd,"Yes\n",4);
         fp1 = fopen("rrshusers.txt","r");
-
+	int auth = 0;
    do {
       c = fscanf(fp1,"%s",oneword); /* got one word from the file */
-      printf("%s\n",oneword);       /* display it on the monitor  */
+     // printf("%s\n",oneword);       /* display it on the monitor  */
 	if (strcmp(oneword,strtok(buf,"\n"))==0)
 	{	
 		c = fscanf(fp1,"%s",oneword);
 		if (strcmp(oneword,strtok(buf2,"\n"))==0)
 		{
-		   Rio_writen(connfd,"Yessss\n",7);
+		   Rio_writen(connfd,"Login Approved\n",15);
+		   auth = 1;
+		}
+		else
+		{	
+		  Rio_writen(connfd,"Login Failed\n",13);
 		}
 	}
-   } while (c != EOF);              /* repeat until EOF           */
-
+   } while (c != EOF);   
+           /* repeat until EOF           */
+	printf("asdasd\n");
+	if (auth == 0)
+	{
+		Rio_writen(connfd,"Login Failed\n",13);	
+	}
     fclose(fp1);
 
 
